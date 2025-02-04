ac766b9465c23496884b72084fc8d5f9bd575a31
Hannah Noelia <58449528+hannahNchan@users.noreply.github.com>
2025-02-04 16:20:33 -0600

    added aws api graphQL

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "improvedwinner": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
